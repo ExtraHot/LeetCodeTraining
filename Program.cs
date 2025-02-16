3114ee5d5a328bf94661fa0ae02c74f8213ae8f3
Mr.Bredman <kartyshov1990@mail.ru>
2025-02-16 20:07:02 +0300

    1) Comple task Palindrome

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeTraining.Tasks;
+
+namespace LeetCodeTraining;
+
+ public static class Program 
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Choose an task 1 - Palindrome");
+        string? choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine(Palindrome.IsPalindrome(120).ToString());
+                break;
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+    }
+}
+
---
+++ Tasks/Palindrome.cs
@@ -0,0 +1,20 @@
+
+//https://leetcode.com/problems/palindrome-number/
+namespace LeetCodeTraining.Tasks;
+ 
+static class Palindrome
+{
+    public static bool IsPalindrome(int x)
+    {
+        int original = x;
+        int reversed = 0;
+
+        if (x < 0) return false;
+        while (x != 0)
+        {
+            reversed = reversed * 10 + (x % 10);
+            x /= 10;
+        }
+        return reversed == original;
+    }
+}
